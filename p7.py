7ccd1e24f8d71142b9f6d88ca8b5b8c067b65abb
Meenal Patro <meenalpatro@gmail.com>
2026-02-03 08:52:44 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,8 @@
+#WAP to take input from user and print
+a=input("Enter your name:")
+b=input("Enter your age:")
+c=input("Enter your roll no.:")
+d=input("Enter your course name:")
+e=input("Enter your college name:")
+
+print("My name is"+a+ "\nMy age is"+b+ "\nMy roll no is"+c+ "\nCurrently I am pursuing"+d+ "\nfrom"+e)
